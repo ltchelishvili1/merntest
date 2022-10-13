adcf5e57bf16161ecc2da5f8dbe023785b71417e
ltchelishvili1 <tchelishvililevani@gmail.com>
2022-10-13 21:55:30 +0400

    input element updates

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+
+import Input from "../../shared/FormElements/Input";
+import {VALIDATOR_REQUIRE} from '../../shared/utils/validators'
+import "./NewPlace.css";
 
 const NewPlace = () => {
   return (
-    <div>NewPlace</div>
-  )
-}
+    <form className="place-form">
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="please enter a valid title"
+      />
+    </form>
+  );
+};
 
-export default NewPlace
+export default NewPlace;
--- src/shared/components/UIElements/Map.js
+++ src/shared/components/UIElements/Map.js
@@ -1,26 +1,9 @@
 import React from 'react'
-import GoogleMapReact from 'google-map-react';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
-const Map = (props) => {
-
-
+const Map  = () => {
   return (
-    <div className={`map ${props.className}`} style={{ height: '100vh', width: '100%' }}>
-         <GoogleMapReact
-        bootstrapURLKeys={{ key: "" }}
-        defaultCenter={props.center}
-        defaultZoom={props.zoom}
-      >
-        <AnyReactComponent
-          lat={props.center.lat}
-          lng={props.center.lng}
-          text="My Marker"
-        />
-      </GoogleMapReact>
-    </div>
+    <div>Map </div>
   )
 }
 
-export default Map
+export default Map 
